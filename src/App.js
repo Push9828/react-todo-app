55b85a25882051624305978166f4193e763b5fd2
Pushkar <pushkar.thakur28@gmail.com>
2020-12-14 22:22:32 +0530

    Impliment filter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
@@ -6,6 +6,27 @@ import TodoList from './components/TodoList';
 function App() {
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filterTodos, setFilterTodos] = useState([]);
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilterTodos(todos.filter((todo) => todo.completed === true));
+        break;
+
+      case 'uncompleted':
+        setFilterTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilterTodos(todos);
+        break;
+    }
+  };
 
   return (
     <div className='App'>
@@ -17,8 +38,9 @@ function App() {
         setTodos={setTodos}
         inputText={inputText}
         setInputText={setInputText}
+        setStatus={setStatus}
       />
-      <TodoList setTodos={setTodos} todos={todos} />
+      <TodoList filterTodos={filterTodos} setTodos={setTodos} todos={todos} />
     </div>
   );
 }
