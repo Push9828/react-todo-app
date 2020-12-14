73950e325f7f6f0ddb57716664531288354db79c
Pushkar <pushkar.thakur28@gmail.com>
2020-12-14 20:22:37 +0530

    Todolist complete and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className='App'>
       <header>
         <h1>Todo List</h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
